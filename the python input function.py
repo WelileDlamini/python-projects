45607d2381b915aa70920c5ca290af075532b4aa
Welile Dlamini <welile.dlamini@ashesi.edu.gh>
2023-01-12 16:41:12 +0000

    update input function

--- the python input function.py
+++ the python input function.py
@@ -1,3 +1,7 @@
 # input() will get user input in console
 # Then print() will print the word 'Hello' and the user input
 print('Hello' +''+ input('What is your name?'))
+
+# OR
+name = input('What is your name?')
+print(f"Hello {name} ");
