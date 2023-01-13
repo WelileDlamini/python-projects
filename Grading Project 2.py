e72e8d1e5191a666b836769400b381b657954f9d
Welile Dlamini <welile.dlamini@ashesi.edu.gh>
2023-01-13 18:48:28 +0000

    Grading Progam

---
+++ Grading Project 2.py
@@ -0,0 +1,31 @@
+# Write a program that converts their scores to grades. By the end of your program, you should have a new dictionary called student_grades that should contain student names for keys and their grades for values. The final version of the student_grades dictionary will be checked.
+# DO NOT write any print statements.
+# This is the scoring criteria:
+# Scores 91 - 100: Grade = "Outstanding"
+# Scores 81 - 90: Grade = "Exceeds Expectations"
+# Scores 71 - 80: Grade = "Acceptable"
+# Scores 70 or lower: Grade = "Fail
+
+student_scores = {
+    "Harry": 81,
+    "Ron": 78,
+    "Hermione": 99,
+    "Draco": 74,
+    "Neville": 62,
+}
+#Create an empty dictionary called student_grades.
+student_grades = {}
+
+# Write your code below to add the grades to student_grades.
+for student in student_scores:
+    score = student_scores[student]
+    if score > 90:
+         student_grades[student] = "Outstanding"
+    elif score > 80:
+         student_grades[student] = "Exceeds Expectations"
+    elif score > 70:
+         student_grades[student] = "Acceptable"
+    else:
+         student_grades[student] = "Fail"
+
+print(student_grades)
