d985b432732f46e88b88b3d5f321132235d8e419
Welile Dlamini <welile.dlamini@ashesi.edu.gh>
2023-03-20 23:12:43 +0000

    Object oriented

---
+++ OOP 11.py
@@ -0,0 +1,7 @@
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def bark(self):
+        print( "Woof!" )
--- OPP Overview.py
+++ OPP Overview.py
@@ -1,3 +1,16 @@
-from prettytable import PrettyTable
-table = PrettyTable()
-print(table)
+# from prettytable import PrettyTable
+# table = PrettyTable()
+# table.add_column("Pokomen Name", ["Pikachu", "Squirtle", "Charmander"])
+# table.add_column("Type",["electric", "Water", "Fire"])
+#
+# # table.align = "l"
+#
+# print(table)
+
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def bark(self):
+        print( "Woof!" )
