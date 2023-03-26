bb4c50338d4458c5788d7b2acbbaa610d3cc8ce8
Welile Dlamini <welile.dlamini@ashesi.edu.gh>
2023-03-26 23:53:13 +0000

    lights

---
+++ car lights.py
@@ -0,0 +1,9 @@
+import time
+
+while True:
+    print("Red light")
+    time.sleep(5) # wait for 5 seconds
+    print("Yellow light")
+    time.sleep(2) # wait for 2 seconds
+    print("Green light")
+    time.sleep(5) # wait for 5 seconds
